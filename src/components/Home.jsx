fb14cf3b1b1966eb2587a62c1d6e485322722505
Alyosha Baryshnikov <alohahoy@gmail.com>
2024-09-28 17:06:40 +0100

    work on the home page to fetch books: issue with the useEffect and user being null

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,9 @@
 // 1. fetch read & unread books
 // 2. display two lists
 
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../contexts/userContext";
+
 // 3. make form for new book addition
 
 // 4. marking book read/unread
@@ -9,9 +12,49 @@
 
 
 const Home = () => {
+
+    const [books, setBooks] = useState([])
+
+    const { user } = useContext(UserContext)
+
+
+
+    useEffect( () => {
+        const getBooks = () => {
+
+            console.log(user)
+
+            fetch(`/api/books/${user.User_id}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            }).then( (res) => {
+                if (!res.ok) {
+                    throw new Error("Could not get books")
+                }
+
+                return res.json()
+            }).then( (data) => {
+                console.log(data)
+                setBooks(data)
+            })
+        }
+
+        console.log(user)
+
+        // getBooks()
+
+    }, [user])
+
     return ( 
         <div>
             <h1>Home Page</h1>
+            {books && books.map( (book) => (
+                <div key={book.book_id}>
+                    <div> {book.title} </div>
+                </div>
+            ))}
         </div>
      );
 }

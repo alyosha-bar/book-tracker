d7fd7123c0db75f5a9cfad693b0ff9a10e22e78b
Alyosha Baryshnikov <alohahoy@gmail.com>
2024-10-01 19:39:01 +0100

    add basic percentage fetching on dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,46 @@
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../contexts/userContext";
 
 
 const Dashboard = () => {
+
+    const [percent, setPercent] = useState();
+
+    const { user } = useContext(UserContext)
+
+    useEffect( () => {
+        
+        const fetchPercent = () => {
+            fetch(`/api/dash/percentage/${user.User_id}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            }).then( (res) => {
+                if (!res.ok) {
+                    // throw new Error("")
+                    console.log("not chilling")
+                }
+                return res.json()
+            }).then( (data) => {
+                console.log(data)
+                setPercent(data.Percentage)
+            }).catch( (err) => {
+                throw new Error(err)
+            })
+        }
+
+        fetchPercent()
+
+
+    }, [user])
+
     return ( 
         <div className="dash-page">
             <h1>Dashboard Page</h1>
+            {percent && (
+                <div> {percent}% read </div>
+            )}
         </div>
      );
 }
